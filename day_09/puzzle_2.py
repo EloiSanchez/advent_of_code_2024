8281658a35617944ac027e3ae6a8920680b0cd40
Eloi Sanchez <eloisanchez16@gmail.com>
2024-12-10 16:56:50 +0100

    Finish P2 day 9

--- day_09/puzzle_2.py
+++ day_09/puzzle_2.py
@@ -11,7 +11,7 @@ def parse_input(characters):
             id = idx // 2
         else:
             id = "."
-        blocks.append([id, int(length)])
+        blocks.extend([[".", 0], [id, int(length)], [".", 0]])
     blocks.append([".", 0])
     return blocks
 
@@ -19,54 +19,24 @@ def parse_input(characters):
 def reorder(blocks: list[list], depth=0):
     idx, r_idx = 0, len(blocks) - 1
     id = 0
-    # changed = False
-
-    # if depth >= 2:
-    #     print(f"depht={depth}")
-    #     return blocks
     while r_idx > 0:
         r_id, r_length = r_block = blocks[r_idx]
         r_idx -= 1
-        # print("doing block", r_block)
         if r_id != ".":
             for idx, block in enumerate(blocks):
                 id, length = block
                 if idx >= r_idx:
                     break
                 if id == ".":
-                    # print("checking", idx, block, r_idx, r_block)
                     if r_length <= length:
-                        # print("Correct, changing...")
-                        # print_block(blocks)
                         blocks.pop(r_idx + 1)
                         blocks.insert(idx, r_block)
-                        # r_idx += 1
-                        # blocks.insert(idx, [".", 0])
-                        # blocks[r_idx + 1][1] = blocks[r_idx + 1][1] + r_length
-                        # print(f"{new_length=}")
                         blocks[idx + 1][1] = blocks[idx + 1][1] - r_length
                         if blocks[r_idx + 2][0] == ".":
                             blocks[r_idx + 2][1] = blocks[r_idx + 2][1] + r_length
                         else:
                             blocks.insert(r_idx + 1, [".", r_length])
-                        # print_block(blocks)
                         break
-    #     print("done block", r_block)
-    # print("finished", blocks)
-    # merged = []
-    # for idx, block_0 in enumerate(blocks):
-    #     if block_0[0] != ".":
-    #         merged.append(block_0.copy())
-    #     else:
-    #         if merged[-1][0] == ".":
-    #     merged[-1][1] += block_0[1]
-    # else:
-    #     merged.append(block_0.copy())
-    # print("mergeddd", merged)
-
-    # if changed:
-    #     return reorder(merged, depth + 1)
-    # return merged
     return blocks
 
 
@@ -83,11 +53,9 @@ def get_checksum(reordered):
     checksum = 0
     for id, length in reordered:
         if id == ".":
-            # print(f"{idx=}, {id=}")
             idx += length
         else:
             for _ in range(length):
-                # print(f"{idx=}, {id=}")
                 checksum += idx * id
                 idx += 1
     return checksum
